21bbb6633bd4415e189c13c892b3d51eef2d2d7e
kanishk <kanishk0422@gmail.com>
2024-10-30 11:11:44 +0530

    updated main.py

--- main.py
+++ main.py
@@ -12,25 +12,23 @@ def process_video(video_path):
     """Pipeline to process a video with transcription, filler removal, and audio alignment."""
 
     output_video_path = "./resources/output_with_corrected_audio.mp4"
+    
+    audio_path = AudioProcessing.extract_audio_from_video(video_path)
 
-    # Step 1: Extract audio from video
-    # audio_path = AudioProcessing.extract_audio_from_video(video_path)
-
-    # Step 2: Transcribe audio and remove filler words
     transcription_service = Transcription()
+    words_with_timestamps = transcription_service.transcribe_audio_with_timestamps(audio_path)
 
-    # words_with_timestamps = transcription_service.transcribe_audio_with_timestamps(audio_path)
     with open("./resources/transcription.json", "r") as file:
         words_with_timestamps = json.load(file)
 
-    # filtered_words = transcription_service.remove_filler_words(words_with_timestamps)
+    filtered_words = transcription_service.remove_filler_words(words_with_timestamps)
 
-    # Step 3: Generate aligned audio
+    with open("./resources/corrected_transcription.json", "r") as file:
+        corrected_transcription = json.load(file)
+
     tts_service = TextToSpeech()
-    # print(words_with_timestamps)
-    processed_audio_path = tts_service.generate_aligned_audio(words_with_timestamps)
+    processed_audio_path = tts_service.generate_aligned_audio(corrected_transcription)
 
-    # Step 4: Merge processed audio with the video
     VideoProcessing.merge_audio_with_video(video_path, processed_audio_path, output_video_path)
     print("Processed video saved at:", output_video_path)
 
